7f0aaaf280d26ebcf06345510c7356fa6d649c4f
Tharushi_Nawodya <159925910+Tharushi-naw@users.noreply.github.com>
2026-08-29 14:49:45 +0530

    Read city codes from JSON

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const citiesData = require("./data/cities.json");
+
 const app = express();
 const PORT = 5000;
 
+const cities = citiesData.List;
+const cityCodes = cities.map(city => city.CityCode);
+
 app.get("/",(req,res) =>{
     res.send("Fidenz Weather API is running")
-});
-
-app.listen(PORT, () => {
-    console.log('Server running on http://localhost:${PORT}');
 });
 
 app.get("/api/status", (req, res) => {
@@ -15,4 +16,15 @@ app.get("/api/status", (req, res) => {
         message: "Weather API is working",
         status: "success"
     });
+});
+
+app.get("/api/cities", (req, res) => {
+    res.json({
+        count: cities.length,
+        cityCodes: cityCodes
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost:${PORT}');
 });
